3b3b5dbd8e62f5f555699a7fa22b3107f1ca183a
Ian Darwin <ian@darwinsys.com>
2015-11-18 14:18:24 -0500

    Sync with database.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 -- Dummy Data
 
-insert into Todo(id, name, createdYear, createdMonth, createdDay, modified) values(-42, 'Get to work!', 2011,11,11, 0);
+insert into Todo(id, name, priority, createdYear, createdMonth, createdDay, modified) values(-42, 'Get back to work!', 'Medium', 2011,11,11, 0);
 
 
