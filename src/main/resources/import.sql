3a2e812814bdb8f946f40499cc8ced2da554baa0
Ian Darwin <ian@darwinsys.com>
2015-11-21 13:19:34 -0500

    Don't need import.sql anymore; add works.

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
--- Dummy Data
-
-insert into Todo(id, name, priority, createdYear, createdMonth, createdDay, modified) values(-42, 'THIS IS THE TEST/DEV DATABASE', 1, 2011,11,11, 0);
-insert into Todo(id, name, priority, createdYear, createdMonth, createdDay, modified) values(-41, 'THIS IS THE TEST/DEV DATABASE', 0, 2011,11,11, 0);
-
-
