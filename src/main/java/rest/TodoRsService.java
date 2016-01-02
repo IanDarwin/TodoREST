4bfcc093e0633af81117b2672b0ea5cee6ff25eb
Ian <ian@darwinsys.com>
2016-01-02 17:34:02 -0500

    Better @Path?

--- src/main/java/rest/TodoRsService.java
+++ src/main/java/rest/TodoRsService.java
@@ -27,7 +27,7 @@ import com.darwinsys.todo.model.Task;
  * It uses a JPA model to access the data.
  * @author Ian Darwin
  */
-@Path("/")
+@Path("")
 @ApplicationScoped
 public class TodoRsService {
 	
