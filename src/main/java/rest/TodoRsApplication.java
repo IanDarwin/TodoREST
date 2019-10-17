49367db6ee282df519e022c2962a621cd64519d0
Ian Darwin <ian@darwinsys.com>
2019-10-17 14:26:22 -0400

    ApplicationPath /rs -> ""

--- src/main/java/rest/TodoRsApplication.java
+++ src/main/java/rest/TodoRsApplication.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/rs")
+@ApplicationPath("")
 public class TodoRsApplication extends Application {
 
 	public TodoRsApplication() {
